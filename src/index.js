852f3e145e04d9f3d74019eb3d7ba1eee9485878
James Jegede <jexzyjames@gmail.com>
2024-03-14 03:57:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router/Router';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId="8730680935-1ms0jbh61vh0afi75oedjn81m88p6pnp.apps.googleusercontent.com">
   <React.StrictMode>
     <Router />
   </React.StrictMode>
+  </GoogleOAuthProvider>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
